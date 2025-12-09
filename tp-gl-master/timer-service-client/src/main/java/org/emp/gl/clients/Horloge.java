395f8ce69bbfa81b7fe936107000fdc9e64c3510
afadel151 <af8356207@gmail.com>
2025-12-09 20:21:55 +0100

    horloge: update

--- tp-gl-master/timer-service-client/src/main/java/org/emp/gl/clients/Horloge.java
+++ tp-gl-master/timer-service-client/src/main/java/org/emp/gl/clients/Horloge.java
@@ -5,16 +5,21 @@ import java.beans.PropertyChangeEvent;
 import org.emp.gl.timer.service.TimerChangeListener;
 import org.emp.gl.timer.service.TimerService;
 import org.emp.Lookup;
+
 public class Horloge implements TimerChangeListener {
 
     String name;
     private final TimerService timerService;
+    int seconds;
+    int minutes;
+    int hours;
 
     public Horloge(String name) {
         Lookup lookup = Lookup.getInstance();
         this.name = name;
-        this.timerService = (TimerService)lookup.getService(TimerService.class);
+        this.timerService = (TimerService) lookup.getService(TimerService.class);
         if (this.timerService != null) {
+
             System.out.println("Horloge " + name + " initialized! with timer service");
             init();
         }
@@ -26,25 +31,60 @@ public class Horloge implements TimerChangeListener {
 
     public void afficherHeure() {
         if (timerService != null) {
-            System.out.println(name + " affiche "
+            seconds = timerService.getSecondes();
+            minutes = timerService.getMinutes();
+            hours = timerService.getHeures();
+            System.out.println(name + "affiche"
                     + timerService.getHeures() + ":"
                     + timerService.getMinutes() + ":"
                     + timerService.getSecondes());
         }
     }
-    
-   
+
+    void secondElapsed() {
+        seconds = (seconds + 1) % 60;
+        if (seconds == 0) {
+            minutes = (minutes + 1) % 60;
+            if (minutes == 0)
+                hours = (hours + 1) % 24;
+        }
+    }
+
     @Override
-    public  void propertyChange(PropertyChangeEvent event)
-    {
+    public void propertyChange(PropertyChangeEvent event) {
         switch (event.getPropertyName()) {
             case SECONDE_PROP:
-                    this.afficherHeure();
+                this.afficherHeure();
+                secondElapsed();
                 break;
-        
+
             default:
                 break;
         }
     }
-        
+
+    public void incrementSecond() {
+        seconds = (seconds + 1) % 60;
+    }
+
+    public void incrementMinutes() {
+        seconds = (minutes + 1) % 60;
+    }
+
+    public void incrementHours() {
+        seconds = (hours + 1) % 24;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
 }
